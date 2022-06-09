8c37c8799eda2ba9c8c3a85a2b86088aaf18b14e
franciowel <spina_francesco@blu.it>
2022-06-09 16:34:42 +0200

    snack 4 controllo dispari

---
+++ jsnack4/js/script.js
@@ -0,0 +1,27 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all’utente di inserire un numero,
+// se è dispari inseriscilo nell’array.
+
+// CREO UNA ARRAY VUOTA
+let yourNumbers = [];
+for(let i=0; i < 6; i++) {
+    // CHIEDO ALL'UTENTE 6 NUMERI
+    yourNumbers.push(prompt('Inserisci un numero. lo farai 6 volte' ));
+}
+
+let oddNumbers = 0;
+// LEGGO I NUMERI
+for(i=0; i < yourNumbers.length; i++) {
+    // LI CONVERTO IN LINGUA MACCHINA 
+    let checkNumb = parseInt(yourNumbers[i]);
+    // CONTROLLO NON SIANO PARI
+    if(checkNumb % 2 !== 0) {
+        let oddNumbers = checkNumb;
+        console.log(oddNumbers)
+        
+    }
+}
+
+ 
+
+// MOSTRO RISULTATO
